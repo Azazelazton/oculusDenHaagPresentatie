a979985fecc63185fb1a8b1e24f9358289d78008
Kay Goossen <kay.goossen@hva.nl>
2015-01-30 20:41:44 +0100

    Broken xbox controls

--- Oculus Demo/Assets/Helicopter.cs
+++ Oculus Demo/Assets/Helicopter.cs
@@ -17,34 +17,31 @@ public class Helicopter : MonoBehaviour {
     [SerializeField]
     float rotorAcceleration = 15;
 
+	[SerializeField]
+	float autoTurnSpeedOnLean = 1;
+
     void Update()
     {
         if (upForce < 0) upForce = 0;
         transform.rigidbody.velocity += upForce * transform.up*Time.deltaTime;
 		rotor.Rotate(new Vector3(0,0,15 * Time.deltaTime * rotorAcceleration)); //rotor.Rotate(new Vector3(0,0,upForce * Time.deltaTime * rotorAcceleration));
 
+		upForce = 9.8f + Input.GetAxis ("Throttle") * 4;
+//        if (Input.GetKey(KeyCode.LeftShift))
+//			upForce = 14;//upForce += acceleration * Time.deltaTime;
+//        else if (Input.GetKey(KeyCode.LeftControl))
+//			upForce = 6;//upForce -= acceleration * Time.deltaTime;
+//		else
+//			upForce = 9.8f;
 
-        if (Input.GetKey(KeyCode.LeftShift))
-			upForce = 14;//upForce += acceleration * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.LeftControl))
-			upForce = 6;//upForce -= acceleration * Time.deltaTime;
-		else
-			upForce = 9.8f;
-
-        if (Input.GetKey(KeyCode.W))
-            transform.Rotate(new Vector3(-rotateSpeed * Time.deltaTime,0,0));
-        if (Input.GetKey(KeyCode.S))
-            transform.Rotate(new Vector3(rotateSpeed * Time.deltaTime, 0, 0));
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(new Vector3( 0, 0,-rotateSpeed * Time.deltaTime));
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(new Vector3(0, 0, rotateSpeed * Time.deltaTime));
+        if (Input.GetAxis("Pitch") != 0) 
+			transform.Rotate(new Vector3(-Input.GetAxis("Pitch") * rotateSpeed * Time.deltaTime,0,0));
+        if (Input.GetAxis("Roll") != 0) 
+			transform.Rotate(new Vector3( 0, 0,Input.GetAxis("Roll") * rotateSpeed * Time.deltaTime));
 
 		Vector3 up = new Vector3(0, 1, 0);
-        if (Input.GetKey(KeyCode.Q))
-            transform.Rotate(up * (-1* rotateSpeed * Time.deltaTime));
-        if (Input.GetKey(KeyCode.E))
-            transform.Rotate(up * (rotateSpeed * Time.deltaTime));
+        if (Input.GetAxis("Yaw") != 0)
+			transform.Rotate(up * (Input.GetAxis("Yaw") * rotateSpeed * Time.deltaTime));
     }
 
     void Reset()
